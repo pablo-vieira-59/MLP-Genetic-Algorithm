308218d10ef876968b028432e38ffdfb84cd5f55
pablo-vieira-59 <46230565+pablo-vieira-59@users.noreply.github.com>
2021-04-27 15:22:33 -0300

    Improved Performance / Fixed Bugs

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,135 +2,135 @@ import numpy as np
 import neural_network as nn
 
 class GeneticAlgorithm():
-    _best_model = None
-    _best_fitness = 0
-    _mutation_rate = 0.1
-    _batch_size = 0
-
-    initial_mutation_rate = 0.1
-    mutation_rate_step = 0.1
-    n_pairs = 0
-    n_population = 0
-    elitism = True
-
-    def __init__(self, initial_mutation_rate :float, mutation_rate_step :float, n_pairs :int, n_population :int, elitism :bool):
+    def __init__(self, initial_mutation_rate :float, mutation_rate_step :float, n_pairs :int, n_population :int, elitism :bool, neural_network_shapes :list):
         self.initial_mutation_rate = initial_mutation_rate
         self.mutation_rate_step = mutation_rate_step
         self.n_pairs = n_pairs
         self.n_population = n_population
         self.elitism = elitism
-        self._mutation_rate = initial_mutation_rate
-        self._batch_size = n_population//n_pairs
-        self._best_fitness = 0
-        self._best_model = None
+        self.mutation_rate = initial_mutation_rate
+        self.batch_size = n_population//n_pairs
+        self.best_fitness = 0
+        self.best_model = None
+        self.network_shapes = neural_network_shapes
     
 
-    def mutate_neurons(self, offspring_individual :list, n_mutations_neurons: int):   
-        mutations_idx = np.random.choice(len(offspring_individual), n_mutations_neurons, replace=False)
+    def __mutate_neurons(self, weights :list, n_mutations_neurons: int):   
+        # Get the index of the neurons to mutate
+        mutations_idx = np.random.choice(len(weights), n_mutations_neurons, replace=False)
+
+        # Create mutations
         for i in range(0, len(mutations_idx)):
-            neuron = offspring_individual[mutations_idx[i]]
-            neuron = (np.random.randint(low=-10000,high=10000,size=len(neuron)) * 0.0001).tolist()
-            offspring_individual[mutations_idx[i]] = neuron
+            weights[mutations_idx[i]] = np.random.rand()
 
-        return offspring_individual
+        return weights
 
 
-    def make_mutations(self, offspring):
-        n_mutations = self._mutation_rate * self.n_population
+    def __make_mutations(self, offspring_weights):
+        # Get the quantity of individuals to mutate
+        n_mutations = self.mutation_rate * self.n_population
         n_mutations = int(n_mutations)
 
-        n_mutations_neurons = self._mutation_rate * len(offspring[0])
+        # Get the quantity of neurons to mutate
+        n_mutations_neurons = self.mutation_rate * len(offspring_weights[0])
         n_mutations_neurons = int(n_mutations_neurons)
 
+        # Get the index of the individuals to mutate
         mutations_idx = np.random.choice(self.n_population, n_mutations, replace=False)
+
+        # Create mutations
         for i in range(0, len(mutations_idx)):
-            offspring[mutations_idx[i]] = self.mutate_neurons(offspring[mutations_idx[i]], n_mutations_neurons)
+            idx = mutations_idx[i]
+            offspring_weights[idx] = self.__mutate_neurons(offspring_weights[idx], n_mutations_neurons)
 
-        return offspring
+        return offspring_weights
 
 
-    def make_cross_over(self, individuals :list, idx :list):
+    def __make_cross_over(self, networks :list, fitness_sorted_indexes :list):
         offspring = []
-        for i in range(0, self.n_pairs * 2, 2):
-            neurons_parent_one = individuals[idx[i]].get_neurons()
-            neurons_parent_two = individuals[idx[i+1]].get_neurons()
-            for j in range(0, self._batch_size):
-                offspring_individual = self.cross_over(neurons_parent_one, neurons_parent_two)
+
+        # Select individuals to reproduce
+        probabilities = np.arange(1,len(fitness_sorted_indexes)+1)[::-1]
+        probabilities = probabilities / sum(probabilities)
+        parents = np.random.choice(fitness_sorted_indexes, self.n_pairs*2, p=probabilities,replace=False)
+
+        # Create offspring using pairs of individuals
+        for i in range(0, len(parents), 2):
+            neurons_parent_one = networks[parents[i]].get_neurons()
+            neurons_parent_two = networks[parents[i+1]].get_neurons()
+
+            # Generate the offspring of the selected pair
+            for j in range(0, self.batch_size):
+                offspring_individual = self.__cross_over(neurons_parent_one, neurons_parent_two)
                 offspring.append(offspring_individual)
+
         return offspring
 
 
-    def cross_over(self, neurons_one :list, neurons_two :list):
+    def __cross_over(self, neurons_one :list, neurons_two :list):
         # Create copy of the first individual
         new_individual = neurons_one.copy()
 
-        # Random copy neurons from second individual
-        qnt_neurons = np.random.randint(low=1,high=len(neurons_one))
+        # Get the quantity of copied neurons from the second individual
+        qnt_neurons = np.random.randint(low=0,high=len(neurons_one))
+
+        # Get the indexes to replace
         idx = np.random.choice(len(neurons_one), qnt_neurons, replace=False)
+
+        # Copies the weight values
         for i in range(0, qnt_neurons):
-            new_individual[idx[i]] = neurons_two[idx[i]].copy() 
+            new_individual[idx[i]] = neurons_two[idx[i]]
         
         return new_individual
 
 
-    def compare_best(self, fitness :list, individuals :list):
+    def __compare_best(self, fitness :list, individuals :list):
         # Gets Best Individuals id's List
         idx = np.argsort(fitness)[::-1]
         best_individual = individuals[idx[0]]
+
         # If has new best Individual
-        if fitness[idx[0]] > self._best_fitness:
+        if fitness[idx[0]] > self.best_fitness:
             # Saves Best Individuals
-            self._best_model = best_individual
-            self._mutation_rate = self.initial_mutation_rate
-            self._best_fitness = fitness[idx[0]]
+            self.best_model = best_individual
+            self.mutation_rate = self.initial_mutation_rate
+            self.best_fitness = fitness[idx[0]]
+
         # Else , Increase Mutation Rate
         else:
-            new_mutation_rate = self._mutation_rate + self.mutation_rate_step
-            if new_mutation_rate <= 0.9:
-                self._mutation_rate = self._mutation_rate + self.mutation_rate_step
-            else:
-                self._mutation_rate = 1
+            self.mutation_rate = self.mutation_rate + self.mutation_rate_step
+
+            if self.mutation_rate > 1:
+                self.mutation_rate = 1
+
         return idx
 
 
-    def create_model(self, offspring_individual :list, individual :nn.NeuralNetwork):
-        new_model = nn.NeuralNetwork()
-        new_model.activations = individual.activations
-        for i in range(0, len(offspring_individual)):
-            offspring_individual[i] = np.array(offspring_individual[i])
+    def __create_models(self, offspring :list):
+        models = []
+        for i in range(0, len(offspring)):
+            new_model = nn.NeuralNetwork().create_from_weights_list(offspring[i], self.network_shapes)
+            models.append(new_model)
 
-        for layer in individual.layers:
-            qnt_neurons = len(layer)
-            layer_neurons = offspring_individual[0:qnt_neurons]
-            new_layer = np.reshape(layer_neurons, layer.shape)
-            new_model.layers.append(new_layer)
-
-            offspring_individual = offspring_individual[qnt_neurons:len(offspring_individual)]
-        return new_model
+        return models
 
 
-    def make_models(self, offspring :list, individuals :list):
-        for i in range(0, len(individuals)):
-            individuals[i] = self.create_model(offspring[i], individuals[i])
-        return individuals
-
-
-    def breed(self,fitness:list,individuals:list):
+    def breed(self, fitness:list, networks:list):
         # Gets Best Individuals id's List
-        idx = self.compare_best(fitness, individuals)
+        idx = self.__compare_best(fitness, networks)
 
         # Makes Cross Over
-        offspring = self.make_cross_over(individuals , idx)
+        offspring = self.__make_cross_over(networks, idx)
 
         # Makes Mutations
-        offspring = self.make_mutations(offspring)
+        offspring = self.__make_mutations(offspring)
 
         # Recreate Models
-        offspring = self.make_models(offspring, individuals)
+        offspring = self.__create_models(offspring)
 
         # Check for Elitism
         if self.elitism:
-            if self._best_model != None:
-                offspring[0] = self._best_model
+            if self.best_model != None:
+                offspring[0] = self.best_model
 
         return offspring
--- neural_network.py
+++ neural_network.py
@@ -1,50 +1,47 @@
 import numpy as np
 
-class Activations():
-    def one_to_one(self, x :list):
-        x = np.multiply(x, -1)
-        x = np.exp(x)
-        x = 1 / (1 + x)
-        x = (x - 0.5) / 0.5
-        return x
-
-    def sigmoid(self, x :list):
-        x = np.multiply(x, -1)
-        x = np.exp(x)
-        x = 1 / (1 + x)
-        return x
-
-    def linear(self, x :list):
-        return x
-
-    def binary(self, x :list):
-        x = np.where(x > 0, 1, 0)
-        return x
-
 class NeuralNetwork():
-    layers = []
-    activations = []
-    
     def __init__(self):
         self.layers = []
-        self.activations = []
 
-    def add_layer(self, in_size, out_size, activation):
-        layer = np.random.randint(low=-1000,high=1000,size=(in_size,out_size)) * 0.0001
+    def add_dense_layer(self, in_size, out_size):
+        layer = np.random.rand(in_size,out_size)
         self.layers.append(layer)
-        self.activations.append(activation)
 
     def predict(self, x :list):
         for j in range(0, len(self.layers)):
             x = np.dot(x, self.layers[j])
-            if j < len(self.layers) - 1:
-                x = x + 1
-            x = self.activations[j](x)
         return x
+
+    def get_shapes(self):
+        shapes = []
+        for l in self.layers:
+            shapes.append(l.shape)
+        return shapes
 
     def get_neurons(self):
         neurons = []
-        for layer in self.layers:
-            for neuron in layer:
-                neurons.append(neuron)
+        for l in self.layers:
+            c = np.ndarray.flatten(l)
+            neurons.append(c)
+
+        neurons = np.concatenate(neurons, axis=0)
         return neurons
+
+    def create_from_shapes(self, shapes :list):
+        for s in shapes:
+            self.add_dense_layer(s[0],s[1])
+        return self
+
+    def create_from_weights_list(self, weights :list,shapes :list):
+        cur_idx = 0
+        new_layers = []
+        for shape in shapes:
+            new_idx = cur_idx + (shape[0]*shape[1])
+            aux = weights[cur_idx:new_idx]
+            aux = np.reshape(aux, shape)
+            new_layers.append(aux)
+            cur_idx = new_idx
+
+        self.layers = new_layers
+        return self
